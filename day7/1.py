24980b8d6a18855ad2452b1836b525db9eb1ba80
Fat3d <56741837+inis17@users.noreply.github.com>
2024-12-07 17:43:20 +0100

    .

--- day7/1.py
+++ day7/1.py
@@ -1,12 +1,36 @@
-file = open('example.txt')
-left_list = []
-right_list = []
-for line in file:
-    a = line.split()
-    left_list.append(int(a[0]))
-    right_list.append(int(a[1]))
-left_list.sort()
-right_list.sort()
+import re
 
-diff = sum(abs(a-b) for a, b in zip(right_list, left_list))
-print(diff)
+
+def calc(first, others, ops, target):
+    for o in ops:
+        res = first
+        if o == '+':
+            res += others[0]
+        if o == '*':
+            res *= others[0]
+        if o == '|':
+            res = int(str(res) + str(others[0]))
+
+        if len(others) == 1:
+            if res == target:
+                return True
+            continue
+        if res > target:
+            continue
+        subset = slice(1, len(others))
+        isTrue = calc(res, others[subset], ops, target)
+        if isTrue:
+            return True
+
+
+# from itertools import product
+tot = []
+file = open('input.txt')
+for line in file:
+    result, *num = map(int, re.findall('\d+', line))
+    operation = ['+', '*', '|']
+    if calc(num[0], num[slice(1, len(num))], operation, result):
+        tot.append(result)
+
+
+print(sum(tot))
