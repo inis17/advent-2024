7900ed6ffeb89d9ee3f0631d9baa94c9a74147b6
Fat3d <56741837+inis17@users.noreply.github.com>
2024-12-11 19:48:10 +0100

    .

--- day11/1.py
+++ day11/1.py
@@ -1,6 +1,28 @@
-file = open('example.txt')
+from collections import Counter
+
+
+file = open('input.txt')
 result = 0
+count = {}
 for line in file:
-    pass
+    count = Counter([int(x) for x in line.split()])
+
+for i in range(0, 75):
+    nextCount = Counter()
+    for key, value in count.items():
+        if key == 0:
+            nextCount[1] += value
+            continue
+        st = str(key)
+        if len(st) % 2 == 0:
+            n = int(len(st)/2)
+            nextCount[int(st[0:n])] += value
+            nextCount[int(st[-n::])] += value
+            continue
+        nextCount[key*2024] = value
+    count = nextCount
+
+result = sum([value for value in count.values()])
+
 
 print(result)
